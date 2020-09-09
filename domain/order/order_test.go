e729c6e230d14044f7adc19f3bc1adf74b427cf2
Emre YAZICI <IEmreYazici@suitsupply.com>
2020-09-09 10:47:05 +0200

    domain tests

--- domain/order/order_test.go
+++ domain/order/order_test.go
@@ -1,1 +1,46 @@
 package order
+
+import (
+	"testing"
+	"time"
+
+	"orderContext/domain/customer"
+	"orderContext/domain/product"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewOrder(t *testing.T) {
+	o := fakeOrder()
+
+	assert.NotNil(t, o)
+}
+
+func TestPayOrder(t *testing.T) {
+	o := fakeOrder()
+
+	o.Pay()
+
+	assert.Equal(t, Paid, o.status)
+}
+
+func TestCancelOrder(t *testing.T) {
+	o := fakeOrder()
+
+	o.Cancel()
+
+	assert.Equal(t, Cancelled, o.status)
+}
+
+func TestShipOrder(t *testing.T) {
+	o := fakeOrder()
+	o.Pay()
+
+	err := o.Ship()
+
+	assert.Nil(t, err)
+}
+
+func fakeOrder() *Order {
+	return NewOrder("123", customer.New(), product.New(), func() time.Time { return time.Now() })
+}
